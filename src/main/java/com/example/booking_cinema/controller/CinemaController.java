678876405112551a83f9c0792a670a66d8579128
Александр Матвеев <aleksandrmatveev@MacBook-Pro-Aleksandr.local>
2022-09-03 15:12:35 +0300

    Initial commit

---
+++ src/main/java/com/example/booking_cinema/controller/CinemaController.java
@@ -0,0 +1,17 @@
+package com.example.booking_cinema.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cinema")
+public class CinemaController {
+
+    @GetMapping("/cinema")
+    public String cinema() {
+        return "";
+    }
+
+}
